53a4dd6d4bff15a69042086cdc5f6edfd47d7653
JumpToSkyFree <alaoui.hamza031@gmail.com>
2026-08-29 19:51:09 +0100

    fix url tooltip show garbage on hover

--- src/av_ui/input_autocomplete_ui.cpp
+++ src/av_ui/input_autocomplete_ui.cpp
@@ -12,6 +12,7 @@
 #include <boost/range/algorithm_ext/push_back.hpp>
 #include <boost/range/iterator_range_core.hpp>
 #include <boost/range/algorithm/find_if.hpp>
+#include <cstdlib>
 
 namespace avUi
 {
@@ -31,7 +32,7 @@ namespace avUi
                  ImGuiInputTextFlags_CallbackHistory;
         const bool changed = ImGui::InputText(label, str, flags, &internal::callback, &state);
         const std::string_view tooltip = *str;
-        ImGui::SetItemTooltip(resolve_vars(tooltip.data(), variables).c_str());
+        ImGui::SetItemTooltip("%s", resolve_vars(tooltip.data(), variables).c_str());
         const ImVec2 itemMin = ImGui::GetItemRectMin();
         const ImVec2 itemMax = ImGui::GetItemRectMax();
         const bool active = ImGui::IsItemActive();
